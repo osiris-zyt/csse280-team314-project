32525eb7b2ff2a24de04eae0292a8cb38191aecb
zhangy35 <zhangy35@rose-hulman.edu>
2020-11-07 00:17:41 -0500

    Firebase Auth & Signout

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -11,22 +11,9 @@ var rhit = rhit || {};
 
 /** globals */
 rhit.variableName = "";
+rhit.fbAuthManager = null;
 
-/** function and class syntax examples */
-rhit.functionName = function () {
-	/** function body */
-};
-
-rhit.ClassName = class {
-	constructor() {
-
-	}
-
-	methodName() {
-
-	}
-}
-
+//manage the mainpage slide effect
 $(document).ready(function() {
   var slideNum = $('.page').length,
     wrapperWidth = 100 * slideNum,
@@ -55,6 +42,7 @@ rhit.startFirebaseUI = function() {
         signInOptions: [
           // Leave the lines as is for the providers you want to offer your users.
           firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+          firebase.auth.EmailAuthProvider.PROVIDER_ID,
           firebase.auth.PhoneAuthProvider.PROVIDER_ID,
           firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
         ],
@@ -75,19 +63,76 @@ rhit.startFirebaseUI = function() {
 
 }
 
-rhit.logAccountInfo = function() {
-  
+rhit.FbAuthManager = class {
+	constructor() {
+		this._user = null;
+	}
+	beginListening(changeListener) {
+		firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        // User is signed in.
+        const displayName = user.displayName;
+        const email = user.email;
+        const phoneNumber = user.phoneNumber;
+        const photoURL = user.photoURL;
+        const isAnonymous = user.isAnonymous;
+        const uid = user.uid;
+        // ...
+        console.log("user signed in ", uid);
+        console.log('email:>>', email);
+        console.log('phoneNumber:>>', phoneNumber);
+        console.log('photoURL:>>', photoURL);
+        console.log('isAnonymous:>>', isAnonymous);
+      } else {
+        console.log("no user signed in");
+        // User is signed out.
+        // ...
+      }
+			this._user = user;
+			changeListener();
+		});
+	}
+	signIn() {
+		Rosefire.signIn("0f3acb1a-0f53-4807-83b7-d1e119140355", (err, rfUser) => {
+			if (err) {
+				console.log("Rosefire error!", err);
+				return;
+			}
+			console.log("Rosefire success!", rfUser);
+
+			// Next use the Rosefire token with Firebase auth.
+			firebase.auth().signInWithCustomToken(rfUser.token).catch((error) => {
+				if (error.code === 'auth/invalid-custom-token') {
+					console.log("The token you provided is not valid.");
+				} else {
+					console.log("signInWithCustomToken error", error.message);
+				}
+      });
+		});
+
+
+	}
+	signOut() {
+		firebase.auth().signOut();
+	}
+	get uid() {
+		return this._user.uid;
+	}
+	get isSignedIn() {
+		return !!this._user;
+	}
+
 }
 
-/* Main */
-/** function and class syntax examples */
-rhit.main = function () {
-  console.log("Ready");
-
+rhit.initializePage = function(){
   if(document.querySelector("#mainPage")){
-    document.querySelector("#lr1").onclick = (event) => {
-      window.location.href = "login.html";
+    //direct to the login page if the user haven't log in
+    if(!rhit.fbAuthManager.isSignedIn){
+      document.querySelector("#lr1").onclick = (event) => {
+        window.location.href = "login.html";
+      }
     }
+    //set the bottom navbar indicator
 	  document.querySelector("#statButton").addEventListener("click", (event) =>{
 	  	document.querySelector("#statIndicator").style.display = "block";
 	  	document.querySelector("#triviaIndicator").style.display = "none";
@@ -97,12 +142,33 @@ rhit.main = function () {
 	  	document.querySelector("#triviaIndicator").style.display = "block";
     });
 
+    document.querySelector("#menuSignOut").addEventListener("click", (event) =>{
+			rhit.fbAuthManager.signOut();
+		});
+
   }
   
   if(document.querySelector("#loginPage")){
-
+    console.log("Ready");
     rhit.startFirebaseUI();
+    document.querySelector("#rosefireBtn").onclick = (event) => {
+			rhit.fbAuthManager.signIn();
+		};
   }
+}
+
+/* Main */
+/** function and class syntax examples */
+rhit.main = function () {
+  console.log("Ready");
+
+  rhit.fbAuthManager = new rhit.FbAuthManager();
+	rhit.fbAuthManager.beginListening(() => {
+    if(document.querySelector("#loginPage") && rhit.fbAuthManager.isSignedIn){
+      window.location.href = "/";
+    }
+		rhit.initializePage();
+	});
 
 };
 
