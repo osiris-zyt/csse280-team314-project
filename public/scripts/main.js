478de611d74b1988939a111cdbcc31b431ce57e4
zhangy35 <zhangy35@rose-hulman.edu>
2020-11-08 22:18:47 -0500

    User Profile created when first login

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -11,7 +11,25 @@ var rhit = rhit || {};
 
 /** globals */
 rhit.variableName = "";
+rhit.FB_COLLECTION_PROFILE = "Profile";
+rhit.FB_KEY_NICKNAME = "Nickname";
+rhit.FB_KEY_AGE = "Age";
+rhit.FB_KEY_COUNTRY = "Country";
+rhit.FB_KEY_STATE = "State"
+rhit.FB_KEY_USER = "User"
+rhit.fbUserManager = null;
 rhit.fbAuthManager = null;
+
+rhit.User = class {
+	constructor(nickname, age, country, state, user){
+		this.nickname = nickname;
+		this.age = age;
+    this.country = country;
+    this.state = state;
+    this.user = user;
+	}
+
+}
 
 //manage the mainpage slide effect
 $(document).ready(function() {
@@ -124,15 +142,66 @@ rhit.FbAuthManager = class {
 
 }
 
-rhit.initializePage = function(){
-  if(document.querySelector("#mainPage")){
-    //direct to the login page if the user haven't log in
-    if(!rhit.fbAuthManager.isSignedIn){
-      document.querySelector("#lr1").onclick = (event) => {
-        window.location.href = "login.html";
-      }
-    }
-    //set the bottom navbar indicator
+rhit.FbUserManager = class {
+	constructor() {
+	  this._documentSnapshots = [];
+	  this._ref = firebase.firestore().collection(rhit.FB_COLLECTION_PROFILE);
+	  this._unsubscribe = null;
+	}
+	
+	add(nickname, age, country, state) {
+		this._ref.add({
+			[rhit.FB_KEY_NICKNAME]: nickname, 
+      [rhit.FB_KEY_AGE]: age, 
+      [rhit.FB_KEY_COUNTRY]: country, 
+			[rhit.FB_KEY_STATE]: state, 
+			[rhit.FB_KEY_USER]: rhit.fbAuthManager.uid, 
+		})
+		.then(function(docRef) {
+			console.log("User profile written with ID: ", docRef.id);
+		})
+		.catch(function(error) {
+			console.error("Error adding profile: ", error);
+		});
+		
+	}
+
+	beginListening(changeListener) {
+		let query = this._ref;
+		this._unsubscribe = query.onSnapshot((querySnapshot) => {
+			console.log("User Profile Update");
+      this._documentSnapshots = querySnapshot.docs;
+			changeListener();
+		})
+	}
+
+	stopListening() {
+		this._unsubscribe();
+	}
+
+	update(nickname, age, country, state) {}
+	delete(id) { }
+
+	get length() {
+		return this._documentSnapshots.length;
+	}
+
+	getUserAtIndex(index) {
+		const docSnapshot = this._documentSnapshots[index];
+		const user = new rhit.User(
+			docSnapshot.get(rhit.FB_KEY_NICKNAME),
+      docSnapshot.get(rhit.FB_KEY_AGE),
+      docSnapshot.get(rhit.FB_KEY_COUNTRY),
+      docSnapshot.get(rhit.FB_KEY_STATE),
+      docSnapshot.get(rhit.FB_KEY_USER)
+		)
+		return user;
+	}
+}
+
+rhit.MainPageController = class {
+	constructor() {
+		//set the bottom navbar indicator
 	  document.querySelector("#statButton").addEventListener("click", (event) =>{
 	  	document.querySelector("#statIndicator").style.display = "block";
 	  	document.querySelector("#triviaIndicator").style.display = "none";
@@ -146,6 +215,81 @@ rhit.initializePage = function(){
 			rhit.fbAuthManager.signOut();
 		});
 
+
+		rhit.fbUserManager.beginListening(this.updateList.bind(this));
+		
+	}
+
+	updateList() {
+    console.log("update list");
+    if(rhit.fbAuthManager.isSignedIn){
+      let profileCreated = false;
+      for (let i = 0; i < rhit.fbUserManager.length; i++){
+        const profile = rhit.fbUserManager.getUserAtIndex(i);
+        console.log(rhit.fbAuthManager.uid);
+        console.log(profile.user);
+        if(profile.user == rhit.fbAuthManager.uid){
+          profileCreated = true;
+        }
+      }
+      if(!profileCreated){
+        rhit.fbUserManager.add("", "", "", "", rhit.fbAuthManager.uid);
+      }
+
+    } 
+  }
+}
+
+rhit.ProfilePageController = class {
+	constructor() {
+		document.querySelector("#applyBtn").addEventListener("click", (event) =>{
+
+		});
+
+		document.querySelector("#deleteBtn").addEventListener("click", (event) =>{
+			
+		});
+
+		rhit.fbUserManager.beginListening(this.updateList.bind(this));
+		
+	}
+
+	updateList() {
+		console.log("update list");
+
+		const newList = htmlToElement('<div id="columns"></div>');
+
+		for (let i = 0; i < rhit.fbPhotoManager.length; i++){
+			const img = rhit.fbPhotoManager.getPhotoAtIndex(i);
+			const newCard = this._createCard(img);
+			newCard.onclick = (event) => {
+				window.location.href = `/photo.html?id=${img.id}`;
+			}
+			newList.appendChild(newCard);
+		}
+
+		const oldList = document.querySelector("#columns");
+		oldList.removeAttribute("id");
+		oldList.hidden = true;
+
+		oldList.parentElement.appendChild(newList);
+
+	}
+
+}
+
+rhit.initializePage = function(){
+  if(document.querySelector("#mainPage")){
+    new rhit.MainPageController();
+    //direct to the login page if the user haven't log in
+    if(!rhit.fbAuthManager.isSignedIn){
+      document.querySelector("#lr1").onclick = (event) => {
+        window.location.href = "login.html";
+      }
+    }else{
+      
+    }
+
   }
   
   if(document.querySelector("#loginPage")){
@@ -155,6 +299,10 @@ rhit.initializePage = function(){
 			rhit.fbAuthManager.signIn();
 		};
   }
+
+  if(document.querySelector("#accountPage")){
+
+  }
 }
 
 /* Main */
@@ -163,10 +311,13 @@ rhit.main = function () {
   console.log("Ready");
 
   rhit.fbAuthManager = new rhit.FbAuthManager();
+
 	rhit.fbAuthManager.beginListening(() => {
     if(document.querySelector("#loginPage") && rhit.fbAuthManager.isSignedIn){
       window.location.href = "/";
     }
+    
+    rhit.fbUserManager = new rhit.FbUserManager();
 		rhit.initializePage();
 	});
 
